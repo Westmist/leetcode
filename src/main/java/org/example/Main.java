acaacdfa641a2ad90b3de80de3b0f75dda2d0863
LiaoCongCong <mingxuannull@gmail.com>
2024-08-06 11:21:49 +0800

    删除链表的倒数第N个节点

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,8 @@ import org.example.book.easy.StringChapter;
 public class Main {
 
     public static void main(String[] args) {
-        LeetCodeInvoke.invoke(ArrayChapter.class);
-        LeetCodeInvoke.invoke(StringChapter.class);
+//        LeetCodeInvoke.invoke(ArrayChapter.class);
+//        LeetCodeInvoke.invoke(StringChapter.class);
         LeetCodeInvoke.invoke(LinkedNodeChapter.class);
     }
 
--- src/main/java/org/example/book/easy/LinkedNodeChapter.java
+++ src/main/java/org/example/book/easy/LinkedNodeChapter.java
@@ -33,4 +33,40 @@ public class LinkedNodeChapter {
     }
 
 
+    /**
+     * @param head 链表
+     * @param n    删除倒数第 n 个
+     * @link {<a href='https://leetcode.cn/leetbook/read/top-interview-questions-easy/xn2925/'>...</a>}
+     * 方法：双指针遍历删除链表指定索引的节点
+     * 时间复杂度：O(n)
+     */
+    @Title("删除链表的倒数第N个节点")
+    @Param(value = {"[1,2,3,4,5]", "2"}, convert = ConvertFactory.ListNodeConvert.class, genericType = {Integer.class})
+    @Answer(value = "[1,2,3,5]", convert = ConvertFactory.ListNodeConvert.class, genericType = Integer.class)
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 两个指针间距为 n, back 节点即为待删除节点的上一个节点
+        // 1、初始化双指针
+        ListNode ans = head;
+        ListNode pre = head;
+        for (int i = 0; i < n; i++) {
+            if (pre == null) {
+                throw new IllegalArgumentException("no nth node");
+            }
+            pre = pre.next;
+        }
+        ListNode back = head;
+        // 2、边界判断，pre == null 即被删除的为头节点
+        if (pre == null) {
+            return ans.next;
+        }
+        // 3、双指针同时前进，直至前指针到达最后一个元素
+        while (pre.next != null) {
+            pre = pre.next;
+            back = back.next;
+        }
+        back.next = back.next.next;
+        return ans;
+    }
+
+
 }
